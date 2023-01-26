7af4c113edb87d657abbe532411b12272f51a90d
choyeang <choyeang0714@gmail.com>
2023-01-26 21:49:42 +0900

    pratice

---
+++ testIfElseIf.java
@@ -0,0 +1,19 @@
+public class testIfElseIf{
+	public static void main(String[] argus) {
+		int x =30;
+		if (x == 10) {
+			System.out.println("x的值是10");
+		}
+		
+	else if (x == 20) {
+		System.out.println("x的值是20");
+	}
+		
+	else if (x == 30) {
+		System.out.println("x的值是30");
+		
+	}else {
+		System.out.println("这是else语句");
+}
+	}
+}
